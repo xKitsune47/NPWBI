e01623e209e783f9972deb863baf474f142f6025
Franciszek 51012 <dsw51012@student.dsw.edu.pl>
2023-05-10 20:35:32 +0200

    reading parsing source files

--- fileconv.py
+++ fileconv.py
@@ -17,15 +17,27 @@ newFileName = input("Specify the name of the new file:\n")
 convType = int(input("Conversion type (1-xml, 2-json, 3-)\n"))
 
 if pConvFormat == 'xml':
+    sourceFile = open(filePath, 'r')
+    xmlObj = xmltodict.parse(sourceFile.read())
     pass
 elif pConvFormat == 'json':
+    sourceFile = open(filePath, 'r')
+    jsonObj = json.load(sourceFile)
     pass
 elif pConvFormat == ('yml' or 'yaml'):
+    sourceFile = open(filePath, 'r')
+    ymlObj = yaml.load(sourceFile, Loader=SafeLoader)
     pass
 
 if convType == 1:
+    outputFile = open('{}.xml'.format(newFileName), 'w')
     pass
 elif convType == 2:
+    outputFile = open('{}.json'.format(newFileName), 'w')
     pass
 elif convType == 3:
+    outputFile = open('{}.yml'.format(newFileName), 'w')
     pass
+
+sourceFile.close()
+outputFile.close()
